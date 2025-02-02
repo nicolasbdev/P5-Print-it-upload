9582983a9ec5b68ad8c72caabfacffb1be86be13
Nicolas Behaegel <nicolas.behaegel@gmx.fr>
2025-02-02 17:23:24 +0100

    JS updated and '_images filed deleted

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+const numberOfSlides = slides.length;
+console.log(numberOfSlides);
+
+const arrowLeft = document.querySelector(".arrow_left");
+
+const arrowRight = document.querySelector(".arrow_right");
+
+let positionImage = 0;
+
+const dots = document.querySelector(".dots");
+
+arrowLeft.addEventListener("click", function()
+{
+ if (positionImage == 0) {
+	positionImage == numberOfSlides - 1;
+ }
+ else (positionImage -- )
+
+} )
+
+
+arrowRight.addEventListener("click", function()
+{
+ if (positionImage == numberofSlides -1 ) {
+	positionImage == 0;
+ }
+ else (positionImage ++ )
+
+} )
+
+
+
